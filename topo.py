6fa7ba10cf72871695fa80078e437a73c06db5e6
Serena Davis <serenajdavis@gmail.com>
2020-12-16 19:37:20 -0800

    Update topo.py
    Updated host names

--- topo.py
+++ topo.py
@@ -7,13 +7,13 @@ class ourTopo(Topo):
         Topo.__init__(self, **opts)
 
         # add hosts
-        host1 = self.addHost("h1")
-        host2 = self.addHost("h2")
-        host3 = self.addHost("h3")
-        host4 = self.addHost("h4")
-        host5 = self.addHost("h5")
-        host6 = self.addHost("h6")
-        host7 = self.addHost("h7")
+        host1 = self.addHost("User")
+        host2 = self.addHost("Intermediary")
+        host3 = self.addHost("Zoom")
+        host4 = self.addHost("Skype")
+        host5 = self.addHost("Email")
+        host6 = self.addHost("Netflix")
+        host7 = self.addHost("Hulu")
 
         # add switches
         switch1 = self.addSwitch("s1")
