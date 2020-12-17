ed3da7034403fbcd39ee7edf0fe774e9c8b7d401
Serena Davis <serenajdavis@gmail.com>
2020-12-17 11:44:57 -0800

    Update and rename receive_intermediary.py to intermediary.py

--- intermediary.py
+++ intermediary.py
@@ -1,13 +1,10 @@
-
-#!/usr/bin/env python
 import sys
 import struct
 import os
 import socket
 import time
 
-DPORT = 1234
-# DPORT = 0x56ce
+PORT = 1234
 
 app_type_to_bandwidth = {1: 800, 2: 512, 3: 10, 4: 10, 5: 10}
 app_type_to_priority = {1: 1, 2: 1, 3: 2, 4: 3, 5: 3}
@@ -17,7 +14,7 @@ app_type_to_host = {0: '10.0.0.1', 1: '10.0.0.3', 2: '10.0.0.4', 3: '10.0.0.5', 
 def main():
      while True:
          s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-         s.bind(('', DPORT))
+         s.bind(('', PORT))
          data, clientAddress = s.recvfrom(200)
          app_type, = struct.unpack('>I', data)
 
@@ -27,7 +24,7 @@ def main():
              time_delay = 1/app_type_to_bandwidth[app_type]*(app_type_to_priority[app_type]**2)
              time.sleep(time_delay)
          data1 = struct.pack('>I', app_type)
-         s.sendto(data1, (app_type_to_host[app_type], DPORT))
+         s.sendto(data1, (app_type_to_host[app_type], PORT))
 
 if __name__ == '__main__':
     main()
