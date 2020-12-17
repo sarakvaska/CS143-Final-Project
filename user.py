d58b34df4fbd2a1e0394aa4d34ce428a04e63586
Serena Davis <serenajdavis@gmail.com>
2020-12-17 11:53:07 -0800

    Update and rename send_app.py to user.py

--- user.py
+++ user.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 import argparse
 import sys
 import socket
@@ -10,30 +8,31 @@ import readline
 import numpy as np
 import time
 
-DPORT = 1234 # MY_PORT 0x04d2
+PORT = 1234
+
+# Dictionary linking app type code to app name
 app_type_to_name = {0: 'User App', 1: 'Zoom', 2: 'Skype', 3: 'Email', 4: 'Netflix', 5: 'Hulu'}
 
 def main():
 
-    if len(sys.argv)<4:
-        print 'pass 3 arguments: <destination IP> <srcPort> <app_type>'
+    if len(sys.argv)<3:
+        print 'pass 2 arguments: <destination IP> <app_type>'
         exit(1)
 
     addr = socket.gethostbyname(sys.argv[1])
-    srcPort = int(sys.argv[2])
-    app_type = int(sys.argv[3])
+    app_type = int(sys.argv[2])
 
+    # Send packet to application through intermediary (representing user uploading to app)
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.bind(('', srcPort))
-    # app_type = application type (zoom, skype, email, netflix, hulu)
+    s.bind(('', PORT))
     data = struct.pack('>I', app_type)
-    s.sendto(data, (addr, DPORT))
-
+    s.sendto(data, (addr, PORT))
+    
+    # Receive incoming packets (representing user downloading from app)
     while True:
         data1, clientAddress = s.recvfrom(200)
         app_type, = struct.unpack('>I', data1)
         print "App Type:", str(app_type), "Host Address:", str(clientAddress), "App Type Name:", app_type_to_name[app_type]
-
 
 if __name__ == '__main__':
     main()
