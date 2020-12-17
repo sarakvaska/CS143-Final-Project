57dd442f1293b4f28dad1d6192eb48579dbeeb1f
Serena Davis <serenajdavis@gmail.com>
2020-12-17 11:50:06 -0800

    Update application.py

--- application.py
+++ application.py
@@ -7,7 +7,7 @@ import time
 PORT = 1234
 
 # Dictionary linking app type code to required user download bandwidth for quality service (Kbps)
-app_type_to_download = {1: 1000, 2: 4000, 3: 10, 4: 3000, 5: 3000}
+app_type_to_download_bandwidth = {1: 1000, 2: 4000, 3: 10, 4: 3000, 5: 3000}
 
 # Dictionary linking app type code to priority, with highest priority being 1 and lowest priority being 3
 app_type_to_priority = {1: 1, 2: 1, 3: 2, 4: 3, 5: 3}
@@ -27,7 +27,7 @@ def main():
 
         # Send outgoing response packet (representing user downloading from app)
         data1 = struct.pack('>I', 0)
-        time_delay = 1 / app_type_to_download[app_type] * (app_type_to_priority[app_type]**2)
+        time_delay = 1 / app_type_to_download_bandwidth[app_type] * (app_type_to_priority[app_type]**2)
         time.sleep(time_delay)
         s.sendto(data1, ('10.0.0.2', PORT))
 
